998c6388ba240168363a44874af1c3915d6ace4f
Nishan <109782567+nishan979@users.noreply.github.com>
2024-02-05 00:27:14 +0600

    ....

--- grade-submission/src/main/java/com/nsn/gradesubmission/GradeController.java
+++ grade-submission/src/main/java/com/nsn/gradesubmission/GradeController.java
@@ -1,13 +1,16 @@
 package com.nsn.gradesubmission;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GradeController {
     
     @GetMapping("/grades")
-    public String getGrades(){
+    public String getGrades(Model model){
+        Grade grade = new Grade("Nishat", "Biology", "A+");
+        model.addAttribute("grade", grade);
         return "grades.html";
     }
 }
