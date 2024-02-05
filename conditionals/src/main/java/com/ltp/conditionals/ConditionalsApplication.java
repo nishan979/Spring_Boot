0ae6da39612420ae9016085d1e996a37a1f71f51
Nishan <109782567+nishan979@users.noreply.github.com>
2024-02-06 01:25:25 +0600

    Conditionals

--- conditionals/src/main/java/com/ltp/conditionals/ConditionalsApplication.java
+++ conditionals/src/main/java/com/ltp/conditionals/ConditionalsApplication.java
@@ -1,4 +1,4 @@
-package com.nsn.conditionals;
+package com.ltp.conditionals;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- conditionals/src/test/java/com/ltp/conditionals/ConditionalsApplicationTests.java
+++ conditionals/src/test/java/com/ltp/conditionals/ConditionalsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nsn.conditionals;
+package com.ltp.conditionals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
