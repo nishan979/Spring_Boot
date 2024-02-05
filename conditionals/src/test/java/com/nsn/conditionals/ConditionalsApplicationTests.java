5f2f24521342504daa98537a22bdeb4717d5503b
Nishan <109782567+nishan979@users.noreply.github.com>
2024-02-06 00:36:43 +0600

    Conditionals

---
+++ conditionals/src/test/java/com/nsn/conditionals/ConditionalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsn.conditionals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConditionalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
