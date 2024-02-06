ec42566ce50d2f47d16dae82133c699a9c517c7e
Nishan <109782567+nishan979@users.noreply.github.com>
2024-02-07 02:16:16 +0600

    ...

--- grade-submission/src/main/java/com/nsn/gradesubmission/GradeController.java
+++ grade-submission/src/main/java/com/nsn/gradesubmission/GradeController.java
@@ -1,5 +1,9 @@
 package com.nsn.gradesubmission;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +12,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 
 public class GradeController {
-    
+
+    // List<Grade> studentGrades = Arrays.asList(
+    //     (new Grade("Rocky", "Biology", "A+")),
+    //     (new Grade("Pinky", "Chemistry", "B+")),
+    //     (new Grade("Roy", "English", "A-"))
+    // );
+    List<Grade> studentGrades = new ArrayList<>();
+
     @RequestMapping("/grades")
     @GetMapping
     public String getGrades(Model model){
-        Grade grade = new Grade("Nishat", "Biology", "A+");
-        model.addAttribute("grade", grade);
+       
+        studentGrades.add(new Grade("Rocky", "Biology", "A+"));
+        studentGrades.add(new Grade("Pinky", "Chemistry", "B+"));
+        studentGrades.add(new Grade("Roy", "English", "A-"));
+        model.addAttribute("grades", studentGrades);
         return "grades";
     }
 }
