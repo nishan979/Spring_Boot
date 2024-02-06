7ca6941669ad98780d92c91a4decb4c891770177
Nishan <109782567+nishan979@users.noreply.github.com>
2024-02-07 02:24:14 +0600

    ...

--- grade-submission/src/main/java/com/nsn/gradesubmission/GradeController.java
+++ grade-submission/src/main/java/com/nsn/gradesubmission/GradeController.java
@@ -13,20 +13,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class GradeController {
 
-    // List<Grade> studentGrades = Arrays.asList(
-    //     (new Grade("Rocky", "Biology", "A+")),
-    //     (new Grade("Pinky", "Chemistry", "B+")),
-    //     (new Grade("Roy", "English", "A-"))
-    // );
-    List<Grade> studentGrades = new ArrayList<>();
+    List<Grade> studentGrades = Arrays.asList(
+        (new Grade("Rocky", "Biology", "A+")),
+        (new Grade("Pinky", "Chemistry", "B+")),
+        (new Grade("Roy", "English", "A-"))
+    );
+    // List<Grade> studentGrades = new ArrayList<>();
 
     @RequestMapping("/grades")
     @GetMapping
     public String getGrades(Model model){
        
-        studentGrades.add(new Grade("Rocky", "Biology", "A+"));
-        studentGrades.add(new Grade("Pinky", "Chemistry", "B+"));
-        studentGrades.add(new Grade("Roy", "English", "A-"));
+        // studentGrades.add(new Grade("Rocky", "Biology", "A+"));
+        // studentGrades.add(new Grade("Pinky", "Chemistry", "B+"));
+        // studentGrades.add(new Grade("Roy", "English", "A-"));
         model.addAttribute("grades", studentGrades);
         return "grades";
     }
