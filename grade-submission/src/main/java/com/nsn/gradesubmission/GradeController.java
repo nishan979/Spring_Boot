0c5970719e83c35f6bd8c2a5f0ed37bef485b7a4
Nishan <109782567+nishan979@users.noreply.github.com>
2024-02-10 13:36:21 +0600

    update

--- grade-submission/src/main/java/com/nsn/gradesubmission/GradeController.java
+++ grade-submission/src/main/java/com/nsn/gradesubmission/GradeController.java
@@ -6,9 +6,10 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
-
 public class GradeController {
 
     List<Grade> studentGrades = Arrays.asList(
@@ -18,7 +19,15 @@ public class GradeController {
     (new Grade("Roy", "English", "A-"))
     );     
 
+    @GetMapping("/")
+    public String gradeForm(Model model) {
+        model.addAttribute("grade", new Grade());
+        return "form";
+    }
     
+
+
+
     @GetMapping("/grades")
     public String getGrades(Model model){
        
