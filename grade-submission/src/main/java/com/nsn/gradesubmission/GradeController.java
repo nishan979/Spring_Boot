3f6f2951a6a7a79eecd4cbe8ddc41c4db994966d
Nishan <109782567+nishan979@users.noreply.github.com>
2024-02-12 22:13:57 +0600

    Update GradeController.java

--- grade-submission/src/main/java/com/nsn/gradesubmission/GradeController.java
+++ grade-submission/src/main/java/com/nsn/gradesubmission/GradeController.java
@@ -1,28 +1,34 @@
 package com.nsn.gradesubmission;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
 public class GradeController {
 
-    List<Grade> studentGrades = Arrays.asList(
-        
-    (new Grade("Rocky", "Biology", "A+")),
-    (new Grade("Pinky", "Chemistry", "B+")),
-    (new Grade("Roy", "English", "A-"))
-    );     
+    List<Grade> studentGrades = new ArrayList<>();   
 
     @GetMapping("/")
-    public String gradeForm(Model model) {
+    public String gradeForm(Model model, @RequestParam(required = false) String name) {
+        System.out.println(name);
+        Grade grade = new Grade();
         model.addAttribute("grade", new Grade());
         return "form";
+    }
+    
+    @PostMapping("/handleSubmit")
+    public String submitGrade(Grade grade) {
+        studentGrades.add(grade);
+        
+        return "redirect:/grades";
     }
     
 
@@ -33,5 +39,13 @@ public class GradeController {
        
         model.addAttribute("grades", studentGrades);
         return "grades";
+    }
+
+    public Integer getGradeIndex(String name) {
+        for (int i = 0; i < studentGrades.size(); i++) {
+            if (studentGrades.get(i).getName().equals(name)) {
+                return i;
+            }
+        }
     }
 }
