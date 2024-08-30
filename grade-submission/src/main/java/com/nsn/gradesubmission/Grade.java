6e26baded17b79dcc605d9e1c84a682caf3d12f4
Nishan <109782567+nishan979@users.noreply.github.com>
2024-08-30 23:53:46 +0600

    Update Grade.java

--- grade-submission/src/main/java/com/nsn/gradesubmission/Grade.java
+++ grade-submission/src/main/java/com/nsn/gradesubmission/Grade.java
@@ -40,5 +40,5 @@ public class Grade {
     public void setScore(String score) {
         this.score = score;
     }
-    //111122211222333112233444112233445511
+    //11112221122233311223344411223344551122
 }
